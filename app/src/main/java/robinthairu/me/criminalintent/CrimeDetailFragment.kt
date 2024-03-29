80f814350cf50c361f35c76254bdc53a35738d0d
thai-ru <robinthairu34@gmail.com>
2024-03-29 05:29:59 +0300

    fragment class

--- app/src/main/java/robinthairu/me/criminalintent/CrimeDetailFragment.kt
+++ app/src/main/java/robinthairu/me/criminalintent/CrimeDetailFragment.kt
@@ -1,4 +1,53 @@
 package robinthairu.me.criminalintent
 
-class CrimeDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import robinthairu.me.criminalintent.databinding.FragmentCrimeDetailBinding
+import java.util.Date
+import java.util.UUID
+
+class CrimeDetailFragment: Fragment() {
+    private lateinit var binding: FragmentCrimeDetailBinding
+    private lateinit var crime: Crime
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        crime = Crime(
+            id = UUID.randomUUID(),
+            title = "",
+            date = Date(),
+            isSolved = false
+        )
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            crimeTitle.doOnTextChanged { text, _, _, _ ->
+                crime = crime.copy(title = text.toString())
+            }
+            crimeData.apply {
+                text = crime.date.toString()
+                isEnabled = false
+            }
+
+            crimeSolved.setOnCheckedChangeListener{ _, isChecked ->
+                crime = crime.copy(isSolved = isChecked)
+            }
+        }
+    }
 }
